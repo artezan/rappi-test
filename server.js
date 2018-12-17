c46c419aaaccb505656630f0d284a174e1f598fe
artezan <artezan_015@hotmail.com>
2018-12-17 17:50:03 -0600

    server

---
+++ server.js
@@ -0,0 +1,16 @@
+//Install express server
+const express = require('express');
+var compression = require('compression')
+const path = require('path');
+
+const app = express();
+app.use(compression())
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/BaratonApp'));
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, '/dist/BaratonApp/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(3000);
